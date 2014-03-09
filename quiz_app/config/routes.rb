c53e9784db7fccb4d564f4f5114c394c0713f538
tristantao <tao.tristan@gmail.com>
2014-03-08 21:58:56 -0800

    added route

--- quiz_app/config/routes.rb
+++ quiz_app/config/routes.rb
@@ -1,4 +1,5 @@
 QuizApp::Application.routes.draw do
+    match '/home' => 'static_pages#index.html'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

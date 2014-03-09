6fd6db5211848153f8999a6638c60b43ec76e7f6
tristantao <tao.tristan@gmail.com>
2014-03-09 00:11:57 -0800

    added quizes and cleaned up show

--- quiz_app/app/assets/javascripts/javascript.js
+++ quiz_app/app/assets/javascripts/javascript.js
@@ -1,5 +1,6 @@
 
 var TOTAL_Q_NUM = 4; //Question num starts at 1.
+var ANS_KEY = {1:"E", 2:"A", 3:"B", 4:"A"};
 
 
 function getAnswer(q_num, q_ans) {
@@ -11,10 +12,34 @@ function getAnswer(q_num, q_ans) {
 	nextQuestion(q_num);
 }
 
-$("#q1_box").show();
-
 function currentSolution(current_q_num, q_ans) {
+	/* Changes the color of the quiz_box to {correct:"green", "wrong":"red"}
+	* Loads the next question as well */
+	var correct = ANS_KEY[current_q_num] == q_ans.toUpperCase();
+	var questionBoxID = "#q"+current_q_num+"_box";
 
+	if (correct) {
+		$(questionBoxID).css({'background-color' : 'red',
+			'-webkit-transition': 'background-color 1000ms linear',
+   		'-moz-transition': 'background-color 1000ms linear',
+   		'-o-transition': 'background-color 1000ms linear',
+   	 	'-ms-transition': 'background-color 1000ms linear',
+    	'transition': 'background-color 1000ms linear'
+		});
+	} else {
+		$(questionBoxID).css({'background-color' : 'YellowGreen',
+			'-webkit-transition': 'background-color 1000ms linear',
+   		'-moz-transition': 'background-color 1000ms linear',
+   		'-o-transition': 'background-color 1000ms linear',
+   	 	'-ms-transition': 'background-color 1000ms linear',
+    	'transition': 'background-color 1000ms linear'
+		});
+	} //end else
+
+	var answer_id = "#a" + current_q_num + "_box";
+	$(answer_id).show("slow");
+
+	return false;
 }
 
 function nextQuestion(current_q_num) {
@@ -23,9 +48,10 @@ function nextQuestion(current_q_num) {
 	next_q = parseInt(current_q_num) + 1;
 	if (next_q <= TOTAL_Q_NUM) {
 		next_question_box = "#q" + next_q + "_box";
-		$(next_question_box).slideDown("fast");
+		//alert("next question:" + next_question_box);
+		$(next_question_box).show("slow");
 	} else {
 		alert ("last question!");
-	}
-
+	} //end else
+	return false;
 }
